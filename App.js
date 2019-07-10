61d9b4089c75e03cec8337cc3b3c15a745b631ff
Anakorn Kyavatanakij <anakornk@gmail.com>
2019-07-10 17:29:00 +0800

    can read chain state, tested on android

--- App.js
+++ App.js
@@ -26,6 +26,12 @@ import {
 
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import { Keyring } from '@polkadot/keyring';
+import { u8aToHex } from "@polkadot/util";
+import {
+  bip39Generate,
+  bip39ToSeed,
+  waitReady
+} from "@polkadot/wasm-crypto";
 
 class App extends React.Component {
   constructor(props) {
@@ -33,11 +39,16 @@ class App extends React.Component {
     this.state = {
       blockNumber: 0,
       chainInfo: '',
-      aliceBalance: 0
+      aliceBalance: 0,
+      transactionHash: ''
     }
 
     // Initialise the provider to connect to the local node
-    this.provider = new WsProvider("ws://127.0.0.1:9944");
+    const providerUrl = Platform.select({
+      ios: "ws://localhost:9944",
+      android: "ws://10.0.2.2:9944"
+    });
+    this.provider = new WsProvider(providerUrl);
   }
 
   async componentDidMount() {
@@ -72,21 +83,39 @@ class App extends React.Component {
       aliceBalance
     })
 
-    // transfer to bob
-    const Bob = "5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty";
-    // Construct the keyring after the API (crypto has an async init)
-    const keyring = new Keyring({ type: "sr25519" });
-    // Add alice to our keyring with a hard-deived path (empty phrase, so uses dev)
-    const alice = keyring.addFromUri("//Alice");
-    // Create a extrinsic, transferring 12345 units to Bob
-    const extrinsic = api.tx.balances.transfer(Bob, 12345);
-    // Sign and send the transaction using our account
-    const hash = await extrinsic.signAndSend(alice);
+    // await waitReady();
+
+    // // transfer to bob
+    // const Bob = "5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty";
+    // // Construct the keyring after the API (crypto has an async init)
+    // const keyring = new Keyring({ type: "sr25519" });
+    // // // // Add alice to our keyring with a hard-deived path (empty phrase, so uses dev)
+    // try {
+    //   const alice = keyring.addFromUri("//Alice");
+    //   // // // Create a extrinsic, transferring 12345 units to Bob
+    //   const extrinsic = api.tx.balances.transfer(Bob, 12345);
+    //   // // Sign and send the transaction using our account
+    //   const hash = await extrinsic.signAndSend(alice);
+    // }
+    // catch(e) {
+    //   this.setState({
+    //     chainInfo: e.toString()
+    //   })
+    // }
+
+    // generate phrase
+    // const phrase = bip39Generate();
+ 
+    // // get ed25519 seed from phrase
+    // const seed = bip39ToSeed('test', '');
+    // this.setState({
+    //   transactionHash: phrase
+    // })
 
   }
 
   render() {
-    let { blockNumber, chainInfo, aliceBalance } = this.state;
+    let { blockNumber, chainInfo, aliceBalance, transactionHash } = this.state;
 
     return (
       <Fragment>
@@ -116,9 +145,9 @@ class App extends React.Component {
                 </Text>
               </View>
               <View style={styles.sectionContainer}>
-                <Text style={styles.sectionTitle}>Learn More</Text>
+                <Text style={styles.sectionTitle}>Transfer</Text>
                 <Text style={styles.sectionDescription}>
-                  Read the docs to discover what to do next:
+                  { transactionHash }
                 </Text>
               </View>
               <LearnMoreLinks />
